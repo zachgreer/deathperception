f6c265678c6bec68d1f63c8092e6ab8de7f0ea36
gaver10 <grapp10@gmail.com>
2013-11-21 11:43:07 -0500

    Added basic menu, framework for level ender

---
+++ Assets/Scripts/Gameplay/Level/LevelEnder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelEnder : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			//call level end
+		}
+	
+	}
+}
---
+++ Assets/Scripts/Gameplay/Menu/StartGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	void OnMouseDown(){
+		
+		Application.LoadLevel (1);
+	}
+}
