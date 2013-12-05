81585a9b122380cc1758b717d2a4abe079a02afc
Zach Greer <zchgre@gmail.com>
2013-12-04 19:59:24 -0500

    Added script that cleans up waves after all of their contained enemies are gone.

---
+++ Assets/Scripts/Gameplay/WaveCleanup.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Destroys a wave when it no longer has any children.
+/// </summary>
+public class WaveCleanup : MonoBehaviour
+{
+	private void Awake()
+	{
+		transform = GetComponent<Transform>();
+	}
+	
+	private void Start()
+	{
+		StartCoroutine(DestroyIfChildless());
+	}
+	
+	private IEnumerator DestroyIfChildless()
+	{
+		yield return new WaitForSeconds(checkChildrenInterval);
+		while (true)
+		{
+			if (transform.childCount == 0)
+				Destroy(gameObject);
+			yield return new WaitForSeconds(checkChildrenInterval);
+		}
+	}
+	
+	private const float checkChildrenInterval = 6.0f;
+	[HideInInspector] new private Transform transform;
+}
