a8c1bbb9767013b3e519e920c97d89b769b76aee
Zach Greer <zchgre@gmail.com>
2013-12-05 01:56:25 -0500

    Background no longer spins in third-person mode.

---
+++ Assets/Scripts/Gameplay/Background.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Attach to the animating background to stop it from rotating while in third
+/// person mode.
+/// </summary>
+[AddComponentMenu ("Background/Background")]
+[RequireComponent (typeof(Animation))]
+public class Background : MonoBehaviour
+{
+	private void Awake()
+	{
+		animation = GetComponent<Animation>();
+	}
+
+	private void Start()
+	{
+		ZDrive.Instance.Switching += TogglePause;
+	}
+
+	private void TogglePause()
+	{
+		animation.enabled = !animation.enabled;
+	}
+
+	private void OnDestroy()
+	{
+		ZDrive zdrive = ZDrive.Instance;
+		if (zdrive != null)
+			zdrive.Switching -= TogglePause;
+	}
+
+	[HideInInspector] new private Animation animation;
+}
