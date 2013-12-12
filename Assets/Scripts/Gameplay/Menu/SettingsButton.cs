b8cdd29500233197191ce7ecf2ee0afc0b06d039
gaver10 <grapp10@gmail.com>
2013-12-11 21:49:35 -0500

    Fixed Coding on Start Screen

--- Assets/Scripts/Gameplay/Menu/SettingsButton.cs
+++ Assets/Scripts/Gameplay/Menu/SettingsButton.cs
@@ -10,7 +10,7 @@ public class SettingsButton : MonoBehaviour {
 	public GameObject Check2;
 	public GameObject Check3;
 	public GameObject But1;
-	public GameObject But2;
+
 	
 	void OnMouseDown(){
 		
@@ -22,7 +22,7 @@ public class SettingsButton : MonoBehaviour {
 		Check2.collider.enabled = true;
 		Check3.collider.enabled = true;
 		But1.collider.enabled = false;
-		But2.collider.enabled = false;
+
 		
 		}
 		
